33faac3dcbf701d427ef73edd3acb2ba8a3512cf
Jacqueline Fung <fungjsl@gmail.com>
2022-02-25 13:13:36 -0500

    2/24-25/2022

---
+++ Advanced JS and Data Structures/map.js
@@ -0,0 +1,19 @@
+//difference between 
+
+const fields = {'prop1': 1, 31: 2, 'prop3': 3}; 
+
+console.log(fields[31])
+
+const obj1 = {'prop1': 1}; 
+const obj2 = {'prop2': 2}; 
+
+const badObject = {obj1 : 'Dentist',  obj2: 'Doctor' }
+
+const map1 = new Map(); 
+map1.set('prop1', 1); 
+map1.set('prop2', 2); 
+
+console.log(map1); 
+
+// console.log(map1.get('prop1'))
+
