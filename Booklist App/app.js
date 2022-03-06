5970478f534ecccfac3fbf96a60e023ea420c8a0
Jacqueline Fung <fungjsl@gmail.com>
2022-03-06 14:56:15 -0500

    almost finished

--- Booklist App/app.js
+++ Booklist App/app.js
@@ -48,6 +48,17 @@ class UI{
         }
     }
 
+    static showAlert(message, className){
+        const div = document.createElement('div'); 
+        div.className = `alert alert-${className}`; 
+        div.appendChild(document.createTextNode(message)); 
+        const container = document.querySelector('.container'); 
+        const form = document.querySelector('#book-form'); 
+        container.insertBefore(div, form); 
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+
+    }
+
     static clearFields(){
         document.querySelector('#title').value=''; 
         document.querySelector('#author').value=''; 
@@ -59,6 +70,36 @@ class UI{
 
 
 //Store Class: Handles Storage
+class Store {
+    static getBooks(){
+        let books; //string version
+        if (localStorage.getItem('books') === null){
+            books = []; 
+           
+        } else {
+            books = JSON.parse(localStorage.getItem('books')); 
+        }
+        return books;
+        }
+
+    static addBook(book){
+        const books = Store.getBooks(); 
+        books.push(book); 
+        localStorage.setItem('books', JSON.stringify(books)0; 
+    }
+
+    static removeBook(isbn){
+        const books = Store.getBooks(); 
+        books.forEach((book, index) => {
+            if (book.isbn === isbn){
+                books.splice(index, 1); 
+            }
+        })
+
+        localStorage.setItem('books', JSON.stringify(books)); 
+
+    }
+}
 
 //Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
@@ -74,23 +115,38 @@ document.querySelector('#book-form').addEventListener('submit', (e)=>
         const isbn = document.querySelector('#isbn').value; 
         
     
+        //validate
+        if(title === '' || author === ''|| isbn === ''){
+            // UI.showAlert('Please fill in all fields', 'danger');
+            alert("Please fill in all fields");
+        } else {
+            //instatiate book
+            const aBook = new Book(title, author, isbn); 
+        
+            //ADD BOOK TO UI 
+            UI.addBookToList(aBook);
 
-        //instatiate book
-        const aBook = new Book(title, author, isbn); 
-    
-        //ADD BOOK TO UI 
-        UI.addBookToList(aBook);
+            //Show success message
+            // UI.showAlert('Book Added','success');
+            alert('book added');
 
-        //Clear fields 
-        UI.clearFields();
+            //Clear fields 
+            UI.clearFields();
+            
+        }
+       
 
 });
 
-//#Event: Remove a book
+//Event: Remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-    UI.deleteBook(e.target)
+    UI.deleteBook(e.target);
+
+    // UI.showAlert('book removed', 'success');
+    alert('book removed');
 
 })
+
 
 
 
