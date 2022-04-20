874d04990950d854562615265c06463bd32f0bdf
Jacqueline Fung <fungjsl@gmail.com>
2022-04-19 21:43:02 -0400

    adding button

--- new-vue-crash-course/src/main.js
+++ new-vue-crash-course/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue' //importing root app component from app.vue
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') //passing in app compent and mounting it to div #app in the html file
