093e053bf59477b3426ab1880a01dedfb10782e9
Jacqueline Fung <fungjsl@gmail.com>
2022-04-20 13:30:29 -0400

    toggle add task finsihed

--- new-vue-crash-course/src/App.vue
+++ new-vue-crash-course/src/App.vue
@@ -2,8 +2,10 @@
 
 <template>
 <div class="container">
-  <Header title="Task Tracker" /> 
-  <AddTask @add-task="addTask"/>
+  <Header @toggle-add-task="toggleAddTask" title="Task Tracker" /> 
+  <div v-if="showAddTask">
+    <AddTask @add-task="addTask"/>
+  </div>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/> <!--v bind so that its dynamic-->
 </div>
 
@@ -28,11 +30,15 @@ export default {
   },
   data() {
     return{
-      tasks: []
+      tasks: [], 
+      showAddTask: false
 
     }
   }, 
   methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
     addTask(task){
       this.tasks=[...this.tasks, task]
     },
--- new-vue-crash-course/src/components/Header.vue
+++ new-vue-crash-course/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{title}}</h1>
-        <Button text="Add Task" color="green" /> <!--capital B since it is a componenta-->
+        <Button @toggle-add-task="$emit('toggle-add-task')" text="Add Task" color="green" /> <!--capital B since it is a componenta-->
     </header>
 </template>
 
