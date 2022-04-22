6b1f2ddf773739fc39d92050b14aa3d604f5ae61
Jacqueline Fung <fungjsl@gmail.com>
2022-04-22 13:07:46 -0400

    before adding routes

--- new-travel-app/src/router/index.js
+++ new-travel-app/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+//if we want to get to source folder, we use @ symbol. 
+//the @ symbol takes us to the src folder 
 
+
+//these are the different pages 
 const routes = [
   {
     path: '/',
@@ -10,12 +15,20 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: AboutView
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
+
+//lazy loading doesn't load something until you need it the first time. 
+//so when u first load the page, not everything is loading at the same 
 
 const router = createRouter({
   history: createWebHashHistory(),
