3ded1006f95de4da00eb6c0dd26a1fc3046dd55a
Jacqueline Fung <fungjsl@gmail.com>
2022-04-25 13:29:54 -0400

    initial proj set-up

--- new-vue-crash-course/src/router/index.js
+++ new-vue-crash-course/src/router/index.js
@@ -1,1 +1,18 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import About from '../views/About'
+
+const routes = [
+    {
+        path: '/about', 
+        name: 'About', 
+        component: About,
+
+    },
+]
+
+const router = createRouter ({
+    history: createWebHistory(process.nextTick.BASE_URL), 
+    routes,
+})
+
+
