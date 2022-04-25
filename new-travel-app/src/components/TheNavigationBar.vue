5c6d275e401a91adcf57797910f3b76af077097e
Jacqueline Fung <fungjsl@gmail.com>
2022-04-25 06:39:15 -0400

    finished dynamic routes

--- new-travel-app/src/components/TheNavigationBar.vue
+++ new-travel-app/src/components/TheNavigationBar.vue
@@ -3,14 +3,29 @@
     <!--router view is a component that changes when the page re-renders-->
     <router-link to="/">Home</router-link> 
     <router-link to="/about">About</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/panama">Panama</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
+   <router-link 
+    v-for="destination in destinations" 
+    :key="destination.id"
+    :to="`/destination/${destination.id}`"
+    >{{ destination.name }}
+    </router-link>
+    
     
   </div>
-
-      
   </template>
+  
+<script>
+
+import sourceData from '@/data.json'
+
+export default {
+  data(){
+    return {
+      destinations: sourceData.destinations,
+    };
+  },
+  
+ };
+</script>
 
   
--- new-travel-app/src/views/DestinationView.vue
+++ new-travel-app/src/views/DestinationView.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <h2>Destinations {{ $route.params.id }}</h2>
+<section>
+    <h1>{{ destination.name }}</h1>
+    <div class="desintation-details">
+        
+        <img :src="`/images/${destination.image}`" alt="destination.name" />
+        <p>{{ destination.description }}</p>
     </div>
+</section>
 </template>
+
+
+
+<script>
+import sourceData from '@/data.json'
+export default {
+    computed: {
+        destinationId(){
+            return parseInt(this.$route.params.id)
+        },
+            //in script we need to use this. keyword 
+        destination(){ 
+            return sourceData.destinations.find(
+                (destination)=>destination.id === this.destinationId)},
+
+        },
+};
+</script>
